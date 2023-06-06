3370c103bd2128484ed574206f03f773bb18cf2b
R-unic <roleyspersonal@gmail.com>
2023-06-06 09:41:07 -0700

    feat: add topbar

---
+++ src/components/TopBar.vue
@@ -0,0 +1,9 @@
+<template>
+  <navbar class="topbar">
+    <a id="home" href="/">
+      <img id="home-icon" src="https://brainboosters.online/meta.jpg" />
+    </a>
+    <a href="/enroll">Enroll</a>
+    <a href="/contact">Contact</a>
+  </navbar>
+</template>
