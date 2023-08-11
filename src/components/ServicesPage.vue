15c5de8ff5d28665608cd7d2961d50d13ec7b4b6
R-unic <roleyspersonal@gmail.com>
2023-08-11 15:09:41 -0400

    feat: add price table for tutoring on Services page

--- src/components/ServicesPage.vue
+++ src/components/ServicesPage.vue
@@ -1,6 +1,44 @@
 <template>
   <main>
     <h1>Tutoring</h1>
-
+    <br/>
+    <table>
+      <tr>
+        <th>Type</th>
+        <th>Plan</th>
+        <th>Price</th>
+      </tr>
+      <tr>
+        <td>1:1</td>
+        <td>50 min. session</td>
+        <td>$60</td>
+      </tr>
+      <tr>
+        <td>1:1</td>
+        <td>2-3 sessions per week</td>
+        <td>$50 per session</td>
+      </tr>
+      <tr>
+        <td><b class="bold">*</b>1:3</td>
+        <td>50 min. session</td>
+        <td>$50</td>
+      </tr>
+      <tr>
+        <td><b class="bold">*</b>1:3</td>
+        <td>2-3 sessions per week</td>
+        <td>$40 per session</td>
+      </tr>
+      <tr>
+        <td><b class="bold">*</b>1:5</td>
+        <td>50 min. session</td>
+        <td>$45</td>
+      </tr>
+      <tr>
+        <td><b class="bold">*</b>1:5</td>
+        <td>2-3 sessions per week</td>
+        <td>$35 per session</td>
+      </tr>
+    </table>
+    <b class="bold">*</b> <i>Based on availability of students with similar needs</i>
   </main>
 </template>
