6a95a82c6e9f76a8c522e2a1525e748a071b4c27
R-unic <roleyspersonal@gmail.com>
2023-08-11 15:31:03 -0400

    feat: fill out all enrichment classes

--- src/components/ServicesPage.vue
+++ src/components/ServicesPage.vue
@@ -40,5 +40,154 @@
       </tr>
     </table>
     <b class="bold">*</b> <i>Based on availability of students with similar needs</i>
+    <br/><br/><br/><br/>
+    <h1>Homeschool Enrichment Classes</h1>
+    <br/>
+    <div class="enrichment-class">
+      <h2>
+        <div style="display: flex; gap: 12px;">
+          <u class="bold">Back to School Bootcamp (K-2)</u>
+          — 2 hour class
+          <b class="bold push-right">$100</b><b>(3 student minimum to offer class)</b>
+        </div>
+      </h2>
+      <p class="desc">
+        &emsp;Review following directions, grade appropriate phonics skills, grade appropriate math skills, grade appropriate writing practice, story-time listening skills.
+      </p>
+    </div>
+    <br/>
+    <div class="enrichment-class">
+      <h2>
+        <div style="display: flex; gap: 12px;">
+          <u class="bold">Back to School Bootcamp (3-5)</u>
+          — 2 hour class
+          <b class="bold push-right">$100</b><b>(3 student minimum to offer class)</b>
+        </div>
+      </h2>
+      <p class="desc">
+        &emsp;Review following directions, reading comprehension and fluency, multiplication/division, area &amp; perimeter, simple fractions, paragraph writing.
+      </p>
+    </div>
+    <br/>
+    <div class="enrichment-class">
+      <h2>
+        <div style="display: flex; gap: 12px;">
+          <u class="bold">Art &amp; Nature</u>
+          — 2 hour class
+          <b class="bold push-right">$100</b><b>(3 student minimum to offer class)</b>
+        </div>
+      </h2>
+      <p class="desc">
+        &emsp;We will explore using natural objects to create art and using art to represent natural objects. Class may include making tissue paper flowers, coffee filter flowers, prints and patterns using natural objects, twig &amp; leaf people/animals, rock painting, Maple seed dragonflies, acorn suncatchers, pinecone bouquets and many other projects.
+      </p>
+    </div>
+    <br/>
+    <div class="enrichment-class">
+      <h2>
+        <div style="display: flex; gap: 12px;">
+          <u class="bold">Classic Art</u>
+          — 2 hour class
+          <b class="bold push-right">$100</b><b>(3 student minimum to offer class)</b>
+        </div>
+      </h2>
+      <p class="desc">
+        &emsp;This class will offer traditional art projects such as directed drawings, pointillist landscapes, Picasso style abstract faces, nature sketching, cartoon drawing, found object art, etc.
+      </p>
+    </div>
+    <br/>
+    <div class="enrichment-class">
+      <h2>
+        <div style="display: flex; gap: 12px;">
+          <u class="bold">Drumming &amp; Percussion (K-2)</u>
+          — 2 hour class
+          <b class="bold push-right">$100</b><b>(3 student minimum to offer class)</b>
+        </div>
+      </h2>
+      <p class="desc">
+        &emsp;Learn about multiple percussion instruments including bells, xylophones, cabasas, tambourines, and various types of drums. Understand rhythm, timing and cooperation when playing a piece with other musicians.
+      </p>
+    </div>
+    <br/>
+    <div class="enrichment-class">
+      <h2>
+        <div style="display: flex; gap: 12px;">
+          <u class="bold">Drumming and Percussion (3-5)</u>
+          — 2 hour class
+          <b class="bold push-right">$100</b><b>(3 student minimum to offer class)</b>
+        </div>
+      </h2>
+      <p class="desc">
+        &emsp;Review rhythm, timing and cooperation with an ensemble. Learn about tones, notes and chords. The class culminates with participation in a drumming circle performance.
+      </p>
+    </div>
+    <br/>
+    <div class="enrichment-class">
+      <h2>
+        <div style="display: flex; gap: 12px;">
+          <u class="bold">Scratch Coding (4-8)</u>
+          — 2 hour class
+          <b class="bold push-right">$100</b><b>(3 student minimum to offer class)</b>
+        </div>
+      </h2>
+      <p class="desc">
+        &emsp;Learn to code the easy way using Scratch, a beginner-friendly game development platform. Create games with animals/creatures, interactions and processes in an easy-to-understand beginner format.
+      </p>
+    </div>
+    <br/>
+    <div class="enrichment-class">
+      <h2>
+        <div style="display: flex; gap: 12px;">
+          <u class="bold">Python Coding (8-12)</u>
+          — 2 hour class
+          <b class="bold push-right">$100</b><b>(3 student minimum to offer class)</b>
+        </div>
+      </h2>
+      <p class="desc">
+        &emsp;This is a more advanced coding class for those interested in robotics, building websites, automating tasks, data analysis and much more.
+      </p>
+    </div>
+    <br/>
+    <div class="enrichment-class">
+      <h2>
+        <div style="display: flex; gap: 12px;">
+          <u class="bold">Life Skills for Teens (10-12)</u>
+          — 2 hour class
+          <b class="bold push-right">$100</b><b>(3 student minimum to offer class)</b>
+        </div>
+      </h2>
+      <p class="desc">
+        &emsp;This class will include time management strategies, budgeting/taxes/money management, business writing (resumés, thank you notes, cover letters), communication &amp; interpersonal skills for teens and respectfully advocating for yourself.
+      </p>
+    </div>
+    <br/>
+    <div class="enrichment-class">
+      <h2>
+        <div style="display: flex; gap: 12px;">
+          <u class="bold">Community Service &amp; Volunteering</u>
+          — Two 2 hour classes
+          <b class="bold push-right">$150</b><b>(3 student minimum to offer class)</b>
+        </div>
+      </h2>
+      <p class="desc">
+        &emsp;In this class, you will discover how to match your interests and skills with a meaningful and satisfying volunteer or community service placement.
+        <br/>
+        &emsp;The first class includes completing an interests and skills inventory, matching results with local volunteer/community service opportunities, and contacting participating organizations.
+        <br/>
+        &emsp;The second class includes transportation to and from organizations, a one hour volunteer/community service experience, and a debrief/plan for going forward.
+      </p>
+    </div>
+    <br/>
+    <div class="enrichment-class">
+      <h2>
+        <div style="display: flex; gap: 12px;">
+          <u class="bold">Internships and Job Shadowing (11-12)</u>
+          — 2 hour class
+          <b class="bold push-right">$150</b>
+        </div>
+      </h2>
+      <p class="desc">
+        &emsp;In this class, your student will learn how to identify internship and job shadowing opportunities, match their own interests and skills with an appropriate placement, and meet/interview with potential employers.
+      </p>
+    </div>
   </main>
 </template>
